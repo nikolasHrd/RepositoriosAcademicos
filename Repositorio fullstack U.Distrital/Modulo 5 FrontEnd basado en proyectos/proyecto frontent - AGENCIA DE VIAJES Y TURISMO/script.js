7fa82ef8784ad01438886b188bcbee94633a35ae
nikolasHrd <nukoluks@hotmail.com>
2023-05-30 17:12:23 -0500

    30-05-2023

--- Repositorio fullstack U.Distrital/Modulo 5 FrontEnd basado en proyectos/proyecto frontent - AGENCIA DE VIAJES Y TURISMO/script.js
+++ Repositorio fullstack U.Distrital/Modulo 5 FrontEnd basado en proyectos/proyecto frontent - AGENCIA DE VIAJES Y TURISMO/script.js
@@ -1,6 +1,6 @@
-const $openClose = document.getElementById("open-close"),
-      $aside = document.getElementById("aside");
+const $openClose = document.getElementById("open-close");
+const $aside = document.getElementById("aside");
 
-$openClose.addEventListener("click",()=>{
-    $aside.classList.toggle("desplegar")
-})
+$openClose.addEventListener("click", () => {
+  $aside.classList.toggle("hidden");
+});
