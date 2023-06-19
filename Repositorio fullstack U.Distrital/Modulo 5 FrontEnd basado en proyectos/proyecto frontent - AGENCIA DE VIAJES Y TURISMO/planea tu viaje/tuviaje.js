14dc1cf9235e9d7bb7b624ad2462d86e47135e90
nikolasHrd <nukoluks@hotmail.com>
2023-06-18 22:43:57 -0500

    18-06-23

--- Repositorio fullstack U.Distrital/Modulo 5 FrontEnd basado en proyectos/proyecto frontent - AGENCIA DE VIAJES Y TURISMO/planea tu viaje/tuviaje.js
+++
@@ -1,6 +0,0 @@
-const $openClose = document.getElementById("open-close"),
-      $aside = document.getElementById("aside");
-
-$openClose.addEventListener("click",()=>{
-    $aside.classList.toggle("desplegar")
-})
